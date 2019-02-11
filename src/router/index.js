54e291310ba80a4f18a36c59ca730f0a1f9dbc22
psywalker <psywalker09@gmail.com>
2019-02-11 09:56:22 +0300

    Add Router and Redux - 3

--- src/router/index.js
+++ src/router/index.js
@@ -1,19 +1,22 @@
 import React from 'react';
 import { LastLocationProvider } from 'react-router-last-location';
 import { Switch, Route, BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux'
 import Boards from '../components/Boards.jsx';
 import Test from '../components/Test.jsx';
 
 const Main = () => (
-    <BrowserRouter basename='/'>
-        <LastLocationProvider>
-            <div>
-                <Switch>
-                    <Route path="/" exact component={Boards}/>
-                    <Route path="/test" component={Test}/>
-                </Switch>
-            </div>
-      </LastLocationProvider>
-    </BrowserRouter>)
 
-  export default Main;
+        <BrowserRouter basename='/'>
+            <LastLocationProvider>
+                <div>
+                    <Switch>
+                        <Route path="/" exact component={Boards}/>
+                        <Route path="/test" component={Test}/>
+                    </Switch>
+                </div>
+            </LastLocationProvider>
+        </BrowserRouter>
+    )
+
+    export default Main;
