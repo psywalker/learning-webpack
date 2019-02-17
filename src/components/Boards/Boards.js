7d7b836503ae04f500890bd95aec06e0b253a5b9
psywalker <psywalker09@gmail.com>
2019-02-17 16:39:28 +0300

    Add Form for adding a board

--- src/components/Boards/Boards.js
+++ src/components/Boards/Boards.js
@@ -1,13 +1,48 @@
-import React from 'react';
+import React, { Component } from 'react';
 import BoardsList from '../BoardsList/BoardsList'
 import './boards-wrapper.scss';
 
-const Boards = ({ boards }) => (
-  <div className="boards-wrapper boards">
-    {boards.map(board =>
-      <BoardsList name={board.name} key={board.id} />
-    )}
-</div>
-)
+class Boards extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      addListForm: false,
+    }
+    this.openFormAddList = this.openFormAddList.bind(this);
+  } 
+
+  openFormAddList(e) {
+    this.setState({
+      addListForm: !this.state.addListForm,
+    }); 
+  }
+
+  render() {
+
+    const { boards } = this.props;
+    const { addListForm } = this.state;
+    return (
+      <div className="boards-wrapper">
+
+        <div className="boards"> 
+          {boards.map(board =>
+            <BoardsList name={board.name} key={board.id} />
+          )}
+        </div>
+    
+        <div className="board-adding">
+          { !addListForm && (<button className="board-adding__btn" onClick={this.openFormAddList}>+ Добавьте ещё одну колонку</button>)}
+          { addListForm && (<form className="board-adding-form">
+            <input className="board-adding-form__input" placeholder="Ввести заголовок списка" />
+            <button className="board-adding-form__btn_add">Добавьте список</button>
+            <button className="board-adding-form__btn_close" onClick={this.openFormAddList}>✖</button>
+          </form>)}
+        </div>
+  
+      </div>
+    );
+  }
+}
 
 export default Boards;
