37117612e5581aa7af6242a40f27537a2e173fb7
psywalker <psywalker09@gmail.com>
2019-02-19 10:20:05 +0300

    Initial dist subtree commit

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const ExtractTextPlugin = require('extract-text-webpack-plugin');
 module.exports = {
   entry: './src/index.js',
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'docs'),
     filename: 'main.js',
   },
   devServer: {
