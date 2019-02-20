354da410efebc57d00b544842a1d13b3af4ebb12
psywalker <psywalker09@gmail.com>
2019-02-20 11:19:43 +0300

    Add Menus

--- src/components/Task/Task.js
+++ src/components/Task/Task.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import ClickOutside from 'react-click-outside';
 import './tasks.scss';
 
 class Task extends Component {
@@ -11,51 +10,23 @@ class Task extends Component {
     }
   } 
 
-  taskMenuHide = () => {
-    this.setState({ taskEditBtnShow: false });
-  };
-
-  taskMenuToggle = () => {
-    const { taskEditBtnShow } = this.state;
-    this.setState({ taskEditBtnShow: !taskEditBtnShow });
-  };  
+  handleBtnMenuToogle = () => {
+    this.setState({
+      taskEditBtnShow: !this.state.taskEditBtnShow,
+    }); 
+}
 
   render() {
 
-    const { id, text, listId } = this.props;
+    const { id, name } = this.props;
     const { taskEditBtnShow } = this.state;
     return (
       <div className="task">
-        <button className={'task__edit-btn ' + (taskEditBtnShow ? 'task__edit-btn_show' : '')} onClick={this.taskMenuToggle}>
-          <span><i className="fas fa-pen"></i></span>
+        <button className={'task__edit-btn ' + (taskEditBtnShow ? 'task__edit-btn_show' : '')} onClick={this.handleBtnMenuToogle}>
+          <span><i class="fas fa-pen"></i></span>
         </button>
         
-        <h2 className="task__title">{id}: {text}</h2>
-
-        <ClickOutside className="clickOutSide" onClickOutside={this.taskMenuHide}>
-        { taskEditBtnShow && (<div className="task-menu-wrapper">
-
-            <div className="task-menu-wrapper__fade" onClick={this.taskMenuToggle}></div>
-
-            <div className="task-menu-wrapper__inner">
-
-              <div className="task-menu-fields">
-                <textarea className="task-menu-fields__textarea"></textarea>
-                <button className="task-menu-fields__btn">Сохранить</button>
-              </div>
-
-              <ul className="task-menu">
-                <li className="task-menu__item">
-                  <button className="task-menu-fields__btn">
-                    <i class="far fa-file-archive"></i>
-                    Архивировать
-                  </button>
-                </li>
-              </ul>
-
-            </div>
-          </div>)}
-        </ClickOutside>
+        <h2 className="task__title">{id}: {name}</h2>
       </div>
     );
   }
